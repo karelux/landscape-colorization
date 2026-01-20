0f740514922390817bc4a3c9be65ae2e1c8f4ad9
Michał Karelus <77463762+karelux@users.noreply.github.com>
2026-01-20 11:04:56 +0100

    Poprawiona Definicja architektury sieci ColorizationNet

--- model.py
+++ model.py
@@ -4,22 +4,24 @@ import torch.nn as nn
 class ColorizationNet(nn.Module):
     def __init__(self):
         super(ColorizationNet, self).__init__()
-        # Encoder: Kompresja obrazu i ekstrakcja cech
-        self.enc1 = nn.Sequential(
+        # Enkoder
+        self.encoder = nn.Sequential(
             nn.Conv2d(1, 64, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.MaxPool2d(2)
+            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1),
+            nn.ReLU()
         )
-        # Decoder: Dekompresja i rekonstrukcja kanałów kolorów
-        self.dec1 = nn.Sequential(
-            nn.ConvTranspose2d(64, 64, kernel_size=2, stride=2),
+        # Dekoder
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2),
+            nn.ReLU(),
             nn.Conv2d(64, 3, kernel_size=3, padding=1),
             nn.Sigmoid()
         )
 
     def forward(self, x):
-        return self.dec1(self.enc1(x))
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
 
-if __name__ == "__main__":
-    model = ColorizationNet()
-    print("Model U-Net został zdefiniowany pomyślnie.")
+print("Model ColorizationNet został poprawnie zdefiniowany.")
